28c43042b5f251d861116733294af19facfadac5
tanvirahmodrafi <87052044+tanvirahmodrafi@users.noreply.github.com>
2024-08-09 08:53:59 +0600

    up

--- Lcs.cpp
+++ Lcs.cpp
@@ -8,10 +8,8 @@ int lcs(string s1, string s2) {
 
     int dp[m + 1][n + 1];
     
-    for (int i = 0; i <= m; i++)
-    {
-        for (int j = 0; j <= n; j++)
-        {
+    for (int i = 0; i <= m; i++){
+        for (int j = 0; j <= n; j++){
             if(i == 0 || j == 0){
                 dp[i][j] = 0;
             }else if(s1[i-1] == s2[j-1]){
@@ -20,7 +18,6 @@ int lcs(string s1, string s2) {
                 dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
             }
         }
-        
     }
     
     string result = "";
